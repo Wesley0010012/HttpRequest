51268e2a2f899d83f289680f7d2809ffc55fb136
Wesley0010012 <wglaurindo33@gmail.com>
2025-04-15 20:34:23 -0300

    feat: add statuses example

--- examples/example.php
+++ examples/example.php
@@ -5,11 +5,20 @@ use Wesley0010012\HttpRequest\Facades\HttpRequestFacade;
 
 require_once("../vendor/autoload.php");
 
+function convertBoolToText(bool $value) {
+    return $value ? 'Yes' : 'No';
+}
+
 HttpRequestFacade::singleton();
 
 $response = (fn($i): HttpResponse => $i)(HttpRequestFacade::get("https://www.google.com"));
-$statusCode = $response->getStatusCode()->value;
+$statusCode = $response->getStatusCode();
 $data = $response->getResponse();
 
-echo "Status Code -> " . $statusCode . "\n";
+echo "Status Code -> " . $statusCode->value . "\n";
 echo "Response -> " . $data . "\n";
+echo "Is Informational -> " . convertBoolToText($statusCode->isInformational()) . "\n";
+echo "Is Successful -> " . convertBoolToText($statusCode->isSuccess()) . "\n";
+echo "Is Redirection -> " . convertBoolToText($statusCode->isRedirection()) . "\n";
+echo "Is Client Error -> " . convertBoolToText($statusCode->isClientError()) . "\n";
+echo "Is Server Error -> " . convertBoolToText($statusCode->isServerError()) . "\n";
